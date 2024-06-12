85fb0c607016dee14fc99e7d639561774a6c497b
Shlomi Ben Shushan <shlomiggl@gmail.com>
2024-06-12 16:34:01 +0300

    tuning curves 2

--- nengo/HW/tuning_curves_2.py
+++ nengo/HW/tuning_curves_2.py
@@ -2,22 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import nengo
 
-from nengo.utils.matplotlib import rasterplot
 from nengo.utils.ensemble import tuning_curves
 
+
+subplot_title_fontdict = {'size': 16, 'weight': 'bold'}
+axis_title_fontdict = {'size': 14, 'weight': 'bold'}
+axis_values_fontdict = {'size': 12, 'weight': 'bold'}
 
 target = lambda x: x + np.sin(x)
 tau_synapse = 0.01
 
+
 # Simulate for each requested number of neurons
-for n_neurons in [100]:#[1, 10, 50, 1000]:
+for n_neurons in [1, 10, 50, 1000]:
 
     # Model definition
     model = nengo.Network()
     with model:
         input_node = nengo.Node(target)
-        ensemble = nengo.Ensemble(n_neurons=n_neurons, dimensions=1, radius=30)
+        ensemble = nengo.Ensemble(n_neurons=n_neurons, dimensions=1, radius=30, seed=1)
         output_node = nengo.Node(size_in=1)
+
         nengo.Connection(input_node, ensemble)
         nengo.Connection(ensemble, output_node)
 
@@ -30,32 +35,25 @@ for n_neurons in [100]:#[1, 10, 50, 1000]:
 
     # Plot results
     t = sim.trange()
-    fig = plt.figure(figsize=(24, 6))
+    fig = plt.figure(figsize=(18, 8))
+    fig.suptitle(f'Representation of f(x)=x+sin(x) using {n_neurons} neuron{"s" if n_neurons > 1 else ""}',
+                 fontsize=22, fontweight='bold')
 
     ## Plot tuning curves
-    plt.subplot(1, 3, 1)
-    plt.title(f'{ensemble.n_neurons} Neuron Tuning Curves')
-    plt.xlabel('I (mA)')
-    plt.ylabel('a (Hz)')
+    plt.subplot(1, 2, 1)
+    plt.title('Tuning Curves', fontdict=subplot_title_fontdict)
+    plt.xlabel('I (mA)', fontdict=axis_title_fontdict)
+    plt.ylabel('a (Hz)', fontdict=axis_title_fontdict)
     plt.plot(*tuning_curves(ensemble, sim))
 
-    ## Plot encoding
-    plt.subplot(1, 3, 2)
-    plt.title(f'Encoding by {ensemble.n_neurons} Neurons')
-    plt.xlabel('Time')
-    plt.ylabel('Encoded value')
-    plt.plot(t, sim.data[input_probe],'r', linewidth=4)
-    plt.ax = plt.gca()
-    rasterplot(t, sim.data[output_probe], ax=plt.ax.twinx(), use_eventplot=True)
-
     ## Plot decoding
-    plt.subplot(1, 3, 3)
-    plt.title(f'Decoding by {ensemble.n_neurons} Neurons')
-    plt.xlabel('time (s)')
-    plt.ylabel('Encoded value')
-    plt.plot(t, sim.data[input_probe],'r', linewidth=4)
+    plt.subplot(1, 2, 2)
+    plt.title('Function Representation', fontdict=subplot_title_fontdict)
+    plt.xlabel('x', fontdict=axis_title_fontdict)
+    plt.ylabel('f(x)', fontdict=axis_title_fontdict)
+    plt.plot(t, sim.data[input_probe],'r', linewidth=6)
     plt.plot(t, sim.data[output_probe])
 
     ## Display plot
-    fig.tight_layout(pad=5.0)
-    plt.show()
+    # plt.show()
+    plt.savefig(f'tuning_curves_{n_neurons}.png')
